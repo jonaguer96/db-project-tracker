654b20a535b8969d3f85ed79c8a53ab63bf2774d
Jonathan Guerrero <jona.guerrero@live.com>
2025-09-07 21:25:12 -0500

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Jonathan Guerrero on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ db-project-trackerTests/db_project_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerTests.swift
+//  db-project-trackerTests
+//
+//  Created by Jonathan Guerrero on 9/7/25.
+//
+
+import Testing
+@testable import db_project_tracker
+
+struct db_project_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
